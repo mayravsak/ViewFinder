8ef156f4bf73996fe28e211b93e3df074f1f6d8c
Apple <apple@Apples-MacBook-Pro-132.local>
2019-07-29 16:12:42 -0400

    Complete Iterartion 2

--- View Finder/View Finder/NewPhotoViewController.swift
+++ View Finder/View Finder/NewPhotoViewController.swift
@@ -13,7 +13,29 @@ class NewPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
     var imagePicker = UIImagePickerController()
 
     @IBOutlet weak var newImage: UIImageView!
-  
+    @IBOutlet weak var TextBoxTapped: UITextField!
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext{
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            photoToSave.caption = TextBoxTapped.text
+    
+            if let userImage = newImage.image {
+                if let userImageData = userImage.pngData() {
+                    photoToSave.imageData = userImageData
+                }
+            }
+            
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+            
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
     
     @IBAction func takeNewPhotoTapped(_ sender: Any) {
         imagePicker.sourceType = .camera
@@ -30,6 +52,8 @@ class NewPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             newImage.image = selectedImage
+            
+            print(selectedImage)
         }
         //this is to dismiss the screen so we can go back to the main screen
         imagePicker.dismiss(animated: true, completion: nil)
